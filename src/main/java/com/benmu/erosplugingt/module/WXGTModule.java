a40f71423a4f38f94faa91ba1b3f5761ab0ab13d
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-04-11 17:54:30 +0800

    commit  'compile 'org.weex.plugin:processor:1.0.2'' and lintOptions {                                                                 abortOnError false                                                             }

--- src/main/java/com/benmu/erosplugingt/module/WXGTModule.java
+++ src/main/java/com/benmu/erosplugingt/module/WXGTModule.java
@@ -1,5 +1,6 @@
 package com.benmu.erosplugingt.module;
 
+import com.alibaba.weex.plugin.annotation.WeexModule;
 import com.benmu.erosplugingt.manager.GetuiManager;
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.common.WXModule;
@@ -8,6 +9,7 @@ import com.taobao.weex.common.WXModule;
  * Created by liuyuanxiao on 18/4/10.
  */
 
+@WeexModule(name = "gtmodule", lazyLoad = true)
 public class WXGTModule extends WXModule {
     @JSMethod
     public void initGeTui() {
