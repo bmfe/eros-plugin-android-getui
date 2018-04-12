aeacfe41290260d59fd3e009d00c48daa200a420
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-04-12 17:24:56 +0800

    oommit Perfect the details fix: GlideImageLoader

--- src/main/java/com/benmu/erosplugingt/module/WXGTModule.java
+++ src/main/java/com/benmu/erosplugingt/module/WXGTModule.java
@@ -5,14 +5,16 @@ import com.benmu.erosplugingt.manager.GetuiManager;
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.common.WXModule;
 
+import java.util.Map;
+
 /**
  * Created by liuyuanxiao on 18/4/10.
  */
 
-@WeexModule(name = "gtmodule", lazyLoad = true)
+@WeexModule(name = "bmPush", lazyLoad = true)
 public class WXGTModule extends WXModule {
     @JSMethod
-    public void initGeTui() {
+    public void initPush(String parms) {
         GetuiManager.pushInit(mWXSDKInstance.getContext());
     }
 }
