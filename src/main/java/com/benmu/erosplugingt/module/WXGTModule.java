ce7d2c40498d2422b8c51cc56f98c17269389bdd
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-04-23 14:15:38 +0800

    commit getCid

--- src/main/java/com/benmu/erosplugingt/module/WXGTModule.java
+++ src/main/java/com/benmu/erosplugingt/module/WXGTModule.java
@@ -2,7 +2,12 @@ package com.benmu.erosplugingt.module;
 
 import com.alibaba.weex.plugin.annotation.WeexModule;
 import com.benmu.erosplugingt.manager.GetuiManager;
+import com.benmu.framework.constant.WXEventCenter;
+import com.benmu.framework.manager.ManagerFactory;
+import com.benmu.framework.manager.impl.dispatcher.DispatchEventManager;
+import com.benmu.framework.model.WeexEventBean;
 import com.taobao.weex.annotation.JSMethod;
+import com.taobao.weex.bridge.JSCallback;
 import com.taobao.weex.common.WXModule;
 
 import java.util.Map;
@@ -17,4 +22,17 @@ public class WXGTModule extends WXModule {
     public void initPush(String parms) {
         GetuiManager.pushInit(mWXSDKInstance.getContext());
     }
+
+    /**
+     * 获取个推的Cid
+     */
+    @JSMethod
+    public void getCid(JSCallback callback) {
+        WeexEventBean weexEventBean = new WeexEventBean();
+        weexEventBean.setKey(WXEventCenter.EVENT_GETCID);
+        weexEventBean.setContext(mWXSDKInstance.getContext());
+        weexEventBean.setJscallback(callback);
+        ManagerFactory.getManagerService(DispatchEventManager.class).getBus().post(weexEventBean);
+    }
+
 }
